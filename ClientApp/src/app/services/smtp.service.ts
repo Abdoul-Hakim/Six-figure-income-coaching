1284f41c8f26d204ba7cf13f3a6484bd459642dd
Abdoul Hakim <abdoulhakimbilla30@outlook.com>
2020-07-14 15:12:49 +0200

    changes before checkout

--- ClientApp/src/app/services/smtp.service.ts
+++ ClientApp/src/app/services/smtp.service.ts
@@ -12,13 +12,13 @@ export class SmtpService {
   async sendSubscribe(email: string) {
     Email.send({
       Host: 'smtp.elasticemail.com',
-      Username: 'buisness.raghu@gmail.com',
-      Password: '982028D05DEC45E54A1D40ADFEBDDAC56807',
-      To : 'buisness.raghu@gmail.com',
-      From : 'buisness.raghu@gmail.com',
+      Username: 'hakim.billa01@gmail.com',
+      Password: '7ED7F08C6C3C4E39ADA7C3C8E8845059EDDC',
+      To : 'hakim.billa01@gmail.com',
+      From : 'hakim.billa01@gmail.com',
       Subject : 'A new client has registred',
       Body : `<p>A new client with the email ${email} has registred!!!<p>`
-    });
+    }).then(console.log('data'));
   }
 
   async sendUnsubscribe(email: string){
